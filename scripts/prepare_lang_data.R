3ef53a66cfe2e24adeb815728385f18e457480c6
Johannes Zauner <112665672+JZauner@users.noreply.github.com>
2025-10-30 12:38:02 +0100

    add new languages

--- scripts/prepare_lang_data.R
+++ scripts/prepare_lang_data.R
@@ -5,9 +5,12 @@ library(rlang)
 library(dplyr)
 library(readxl)
 library(glue)
+library(yaml)
+
 
 prepare_lang_data <- function(language){
   #loading data
+  prepare_author_data(language)
   path <- "assets/LadenburgConsensuStatements.xlsx"
   sheets <- readxl::excel_sheets(path)
   stopifnot("language argument must be part of the 'LadenburgConsensuStatements.xlsx' worksheets" = 
@@ -20,5 +23,29 @@ prepare_lang_data <- function(language){
   headers <<- names(data)
   names(data) <- c("chapter", "number", "statement", "simplified", "context", "reference")
   dataset <<- data
+}
+
+prepare_author_data <- function(language){
+  path <- "assets/LadenburgConsensuStatements.xlsx"
+  data <- read_xlsx(path, sheet = "Contributors")
+  
+  data2 <- data |> filter(Language == language)
+  
+  authors <- apply(data2, 1, function(x) {
+    entry <- 
+      list(
+        name = paste(x["First name"], x["Last name"]),
+        affiliation = ifelse(!is.na(x["Affiliation"]), x["Affiliation"], NA),
+        orcid = if(!is.na(x["ORCID"])) x["ORCID"]
+      )
+    Filter(Negate(is.null), entry)
+  })
+  
+  # Convert to YAML; authors can be length 1 or many
+  yaml_text <- as.yaml(list(author = authors), indent = 2, line.sep = "\n")
+  # yaml_text <- as.yaml(list(author = authors))
+  # cat(yaml_text)
+  # write to a file Quarto can include
+  writeLines(yaml_text, paste0("assets/translations/authors_", language, ".yml"))
 }
 
